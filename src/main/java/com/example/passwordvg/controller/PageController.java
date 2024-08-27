044efe5a468774f9ac849bf3d9762eb882ea29b3
Martin Harrysson <MartinHarrysson1990@hotmail.com>
2024-08-27 22:44:02 +0200

    added controller to render userinput and display both it hashed to md5 and sha256

--- src/main/java/com/example/passwordvg/controller/PageController.java
+++ src/main/java/com/example/passwordvg/controller/PageController.java
@@ -1,13 +1,18 @@
 package com.example.passwordvg.controller;
 
 
+import com.example.passwordvg.utils.HashAlgorithm;
+import com.example.passwordvg.utils.HashUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class PageController {
+    HashUtils hashUtils = new HashUtils();
 
     @GetMapping("/")
     public String redirect(){
@@ -25,12 +30,16 @@ public class PageController {
     }
 
     @PostMapping("/hash")
-    public String hash(){
+    public String hash(Model model, @RequestParam String textToHash) {
+        String hashedMD5 = hashUtils.hashText(textToHash, HashAlgorithm.MD5);
+        String hashedSHA256 = hashUtils.hashText(textToHash, HashAlgorithm.SHA256);
+        model.addAttribute("hashedMD5", hashedMD5);
+        model.addAttribute("hashedSHA256", hashedSHA256);
         return "home";
     }
 
     @PostMapping("/password")
-    public String password(){
+    public String password(Model model,@RequestParam String textToHash){
         return "crackPassword";
     }
 }
